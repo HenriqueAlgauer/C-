a0bcc347862263afbf991956a9addc51273047b0
henriqueAlgauer <algauer.henrique@gmail.com>
2024-03-11 17:43:35 -0300

    aplicação finalizada

--- primeiro projeto/Program.cs
+++ primeiro projeto/Program.cs
@@ -2,83 +2,94 @@
 
 Dictionary<string, List<int>> bandas = new Dictionary<string, List<int>>();
 
-bandas.Add("Link", new List<int> {10,8,9,7});
-bandas.Add("SOD", new List<int> {1,8,5,7});
-bandas.Add("BS", new List<int> {8,5,7});
+bandas.Add("Link", new List<int> { 10, 8, 9, 7 });
+bandas.Add("SOD", new List<int> { 1, 8, 5, 7 });
+bandas.Add("BS", new List<int> { 8, 5, 7 });
 bandas.Add("Dk", new List<int>());
 
-void ExibirLogo(){
-    Console.WriteLine(@"
-█▀ █▀▀ █▀█ █▀▀ █▀▀ █▄░█   █▀ █▀█ █░█ █▄░█ █▀▄
-▄█ █▄▄ █▀▄ ██▄ ██▄ █░▀█   ▄█ █▄█ █▄█ █░▀█ █▄▀");
-    Console.WriteLine(mensagem);
-}
-
-void ExibirTitulo(string titulo){
-    int qntLetras = titulo.Length;
-    string asteriscos = string.Empty.PadLeft(qntLetras,'*');
-    Console.WriteLine(asteriscos);
-    Console.WriteLine(titulo);
-    Console.WriteLine(asteriscos + "\n");
-}
-
-void RegistrarBanda(){
-    Console.Clear();
-    ExibirTitulo("Registro de banda");
-    Console.Write("Digite o nome da banda que deseja registrar: ");
-    string nomeBanda = Console.ReadLine()!;
-    bandas.Add(nomeBanda, new List<int>());
-    Console.WriteLine($"A banda {nomeBanda} foi registrada.");
-    Thread.Sleep(800);
-    Console.Clear();
-    
-    ExibirMenu();
-}
-
-void MostrarBandas(){
-    Console.Clear();
-    ExibirTitulo("Exibindo todas as bandas registradas =)");
-    foreach(string banda in bandas.Keys){
-        Console.WriteLine($"Banda: {banda}");
-    }
+void VoltarAoMenu(){
     Console.WriteLine("\nDigite uma tecla para voltar ao menu");
     Console.ReadKey();
     Console.Clear();
     ExibirMenu();
 }
 
-void MostrarAvaliacao(){
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+█▀ █▀▀ █▀█ █▀▀ █▀▀ █▄░█   █▀ █▀█ █░█ █▄░█ █▀▄
+▄█ █▄▄ █▀▄ ██▄ ██▄ █░▀█   ▄█ █▄█ █▄█ █░▀█ █▄▀");
+    Console.WriteLine(mensagem);
+}
+
+void ExibirTitulo(string titulo)
+{
+    int qntLetras = titulo.Length;
+    string asteriscos = string.Empty.PadLeft(qntLetras, '*');
+    Console.WriteLine(asteriscos);
+    Console.WriteLine(titulo);
+    Console.WriteLine(asteriscos + "\n");
+}
+
+void RegistrarBanda()
+{
+    Console.Clear();
+    ExibirTitulo("Registro de banda");
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeBanda = Console.ReadLine()!;
+    bandas.Add(nomeBanda, new List<int>());
+    Console.WriteLine($"A banda {nomeBanda} foi registrada.");
+    VoltarAoMenu();
+}
+
+void MostrarBandas()
+{
+    Console.Clear();
+    ExibirTitulo("Exibindo todas as bandas registradas =)");
+    foreach (string banda in bandas.Keys){
+        Console.WriteLine($"Banda: {banda}");
+    }
+    VoltarAoMenu();
+}
+
+void MostrarAvaliacao()
+{
     Console.Clear();
     ExibirTitulo("Mostrando avaliações");
     Console.Write("\nQual banda deseja consultar a avalição: ");
     string banda = Console.ReadLine()!;
-    if(bandas.ContainsKey(banda)){
-        foreach(){
-
-        }
+    if (bandas.ContainsKey(banda)){
+        List<int> notasBanda = bandas[banda];
+        Console.WriteLine($"A média da banda {banda} é: {notasBanda.Average()}");
+        VoltarAoMenu();
+    }else{
+        Console.WriteLine("Essa banda não foi encontrada");
+        VoltarAoMenu();
     }
 }
 
-void AvaliarBanda(){
+void AvaliarBanda()
+{
     Console.Clear();
     ExibirTitulo("Avaliar banda");
     Console.Write("Digite o nome da banda que deseja avaliar: ");
     string nomeBanda = Console.ReadLine()!;
-    if(bandas.ContainsKey(nomeBanda)){
+    if (bandas.ContainsKey(nomeBanda))
+    {
         Console.Write($"Qual a nota deseja avaliar a banda {nomeBanda}: ");
         int nota = int.Parse(Console.ReadLine()!);
         bandas[nomeBanda].Add(nota);
         Console.WriteLine("\nNota registrada com sucesso");
-    }else{
+    }
+    else
+    {
         Console.WriteLine($"\nA banda {nomeBanda} não foi encontrada");
-        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
-        Console.ReadKey();
-        Console.Clear();
-        ExibirMenu();
+        VoltarAoMenu();
     }
 }
 
-void ExibirMenu(){
+void ExibirMenu()
+{
     ExibirLogo();
     Console.WriteLine("\nDigite 1 para registrar uma banda");
     Console.WriteLine("Digite 2 para mostra todas as bandas");
@@ -89,20 +100,21 @@ void ExibirMenu(){
     Console.Write("\nDigite a sua opção: ");
     string opcaoEscolhida = Console.ReadLine()!;
     int numeroEscolhido = int.Parse(opcaoEscolhida);
-    
-    switch(numeroEscolhido){
-        case 1: 
-            RegistrarBanda();break;
-        case 2: 
-            MostrarBandas();break;
-        case 3: 
-            AvaliarBanda();break;
-        case 4: 
-            MostrarAvaliacao();break;
-        case 0: 
-            Console.WriteLine("0");break;
-        default: 
-            Console.WriteLine("Opção inválida");break;
+
+    switch (numeroEscolhido)
+    {
+        case 1:
+            RegistrarBanda(); break;
+        case 2:
+            MostrarBandas(); break;
+        case 3:
+            AvaliarBanda(); break;
+        case 4:
+            MostrarAvaliacao(); break;
+        case 0:
+            Console.WriteLine("0"); break;
+        default:
+            Console.WriteLine("Opção inválida"); break;
     }
 }
 
--- primeiro projeto/obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ primeiro projeto/obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20146e92b4565110387f3dec0e85e839660a0cd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed864fbd895d5b054f69f48b56c0cc048f6efdbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
